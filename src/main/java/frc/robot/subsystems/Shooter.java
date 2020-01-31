bd217a07951856a3fa77e63df327a77df9b89c4b
Jen <jen@baltimorebolts.com>
2020-01-30 20:48:50 -0500

    Added PID Tuner function -DRRM

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,12 +10,22 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
-  CANSparkMax  SMotorChip;
+  CANSparkMax SMotorChip;
   CANSparkMax SMotorDale;
+  double shooterMotorSpeed = 0;
+
+  PIDController shooterPID;
+  double pVal = 1.0; // These may be terrible default values! DRRM 
+  double iVal = 0.5; // These may be terrible default values! DRRM
+  double dVal = 0.5; // These may be terrible default values! DRRM
+  double pidShooterSpeed = 0.0;
 
   /**
    * Creates a new Shooter.
@@ -23,10 +33,59 @@ public class Shooter extends SubsystemBase {
   public Shooter() {
   //SMotorChip = new CANSparkMax(ShooterConstants.SHOOTER_MOTOR_CHIP, MotorType.kBrushless);
   //SMotorDale = new CANSparkMax(ShooterConstants.SHOOTER_MOTOR_DALE, MotorType.kBrushless);
+    shooterPID = new PIDController(pVal, iVal, dVal);
+    shooterPID.setSetpoint(pidShooterSpeed);
+
+    // Prints the initial PID values to smart dashboard
+    SmartDashboard.putNumber("Current pVal = ", pVal);
+    SmartDashboard.putNumber("Current iVal = ", iVal);
+    SmartDashboard.putNumber("Current dVal = ", dVal);
+    SmartDashboard.putNumber("Calculated PID = ", pidShooterSpeed);
+    SmartDashboard.putNumber("Shooter Motor Speed = ", shooterMotorSpeed);
+    SmartDashboard.putBoolean("PID or Value:", false); // Set to true for using "Shooter Motor Speed" to control shooter speed
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    PIDTuner(); // Comment this out once we figure out our PID values.
   }
+
+  public void PIDTuner() {
+    double pTemp = 0;
+    double iTemp = 0;
+    double dTemp = 0;
+    double motorShooterSpeed = 0;
+    boolean pidOrValue;
+
+    pTemp = SmartDashboard.getNumber("Current pVal = ", -1);
+    iTemp = SmartDashboard.getNumber("Current iVal = ", -1);
+    dTemp = SmartDashboard.getNumber("Current dVal = ", -1);
+    motorShooterSpeed = SmartDashboard.getNumber("Shooter Motor Speed = ", -1); 
+    pidOrValue = SmartDashboard.getBoolean("PID or Value:", false);
+
+    // We don't want to set our constants to a negative value so lets prevent that
+    if (pTemp >= 0 ) {
+      pVal = pTemp;
+    }
+
+    if (iTemp >= 0 ) {
+      iVal = iTemp;
+    }
+
+    if (dTemp >= 0 ) {
+      dVal = dTemp;
+    }
+    shooterPID.setPID(pVal, iVal, dVal);
+    pidShooterSpeed = shooterPID.calculate(pidShooterSpeed);
+    SmartDashboard.putNumber("Calculated PID = ", pidShooterSpeed);
+
+    if (pidOrValue) {
+      SMotorChip.set(motorShooterSpeed);
+      SMotorDale.set(-motorShooterSpeed);
+    } else {
+      // Use PID value
+    }
+  }
+
+
 }
