b580537360279f2ee2ce8afa034f06e7b923aee4
Faith <fmauricesaw@gmail.com>
2020-03-04 18:04:05 -0500

    ADDD FW, LM

--- src/main/java/frc/robot/commands/AutonomousShoot.java
+++ src/main/java/frc/robot/commands/AutonomousShoot.java
@@ -9,9 +9,13 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Harvester;
 
 public class AutonomousShoot extends CommandBase {
   Shooter robotShooter;
+  Indexer roboIndexer;
+  Harvester roboHarvester;
   /**
    * Creates a new AutonomousShooter.
    */
@@ -23,11 +27,13 @@ public class AutonomousShoot extends CommandBase {
   @Override
   public void initialize() {
     robotShooter = new Shooter();
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    new FirePowerCell(robotShooter, roboIndexer, roboHarvester);
     robotShooter.SetShooterSpeed(0);
   }
 
